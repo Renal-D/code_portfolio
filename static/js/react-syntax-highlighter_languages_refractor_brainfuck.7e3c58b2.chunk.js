a5968325f6bba1ee294983dec4ce5aa1c9aea801
Renal-D <renaldi31saputra@gmail.com>
2023-05-16 15:50:14 +0700

    Updates

---
+++ static/js/react-syntax-highlighter_languages_refractor_brainfuck.7e3c58b2.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkren=self.webpackChunkren||[]).push([[5539],{2689:function(e){function a(e){e.languages.brainfuck={pointer:{pattern:/<|>/,alias:"keyword"},increment:{pattern:/\+/,alias:"inserted"},decrement:{pattern:/-/,alias:"deleted"},branching:{pattern:/\[|\]/,alias:"important"},operator:/[.,]/,comment:/\S+/}}e.exports=a,a.displayName="brainfuck",a.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_brainfuck.7e3c58b2.chunk.js.map
